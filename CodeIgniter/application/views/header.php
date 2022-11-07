bbe8030a8f4415876b04ad49dfff20c41b1c972c
Mathis MICHENAUD <E211769J@univ-nantes.fr>
2022-11-07 14:23:33 +0100

    view login connexion, assets > icon

---
+++ CodeIgniter/application/views/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+</head>
+<body>
+    <img src="../icon/icon-error.svg" alt="">
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <head>
+    <link rel="stylesheet" href="../styles/reset.css">
+</head>
+
+
+<nav id="nav-container">
+    <img class="icon" src="" alt="">
+    <div class="nav-links">
+        <ul>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">Modèles 3D</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+
+    <div class="user-action">
+        <img class=" "src="../src/icon/icon-shopping-cart.svg" alt="panier">
+        <div class="account">
+            <img src="" alt="">
+        </div>
+    </div>
+</nav> -->
---
+++ CodeIgniter/application/views/inscriptionPage.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Inscription</title>
+</head>
+<body>
+    <div class="logo">
+        <h2> logo</h2>
+    </div>
+
+    <form action="" method="post"></form>
+        <div class="form-head">
+            <h1>Inscription</h1>
+        </div>
+
+        <div class="form-input">
+            <div class="user-info">
+                <div class="name">
+                    <label for="name">Nom</label>
+                    <input class="input" type="text" id="name" require>
+                </div>
+                <div class="first-name">
+                    <label for="first-name">Prénom</label>
+                    <input class="input" type="text" id="first-name" require>
+                </div>
+            </div>
+
+            <div class="email">
+                <label for="email" id="email" pattern=".+@globex\.com" size="30" required>Email</label>
+                <input class="input" type="email" id="email" require>
+            </div>
+
+
+            <div class="password">
+                <label for="password">Mot de passe</label>
+                <input class="input" type="password" id="password" require>
+            </div>
+        </div>
+
+
+        <div class="form-btn">
+            <button type="submit">S'inscrire</button>
+        </div>
+
+        <div class="form-inscription-link">
+            <h4>Vous êtes déjà membre ?</h4>
+            <a href="connexionPage.php">Se connecter</a>
+        </div>
+    </form>
+
+    <div class="bars">
+        <div class="bar-black"></div>
+        <div class="bar-gey"></div>
+        <div class="bar-orange"></div>
+    </div>
+</body>
+</html>
